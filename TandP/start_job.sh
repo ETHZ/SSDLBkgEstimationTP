7490107f4866e9cfc236f38d0363ece2924e5e95
Gael Perrin <gael.ludovic.perrin@cern.ch>
2015-07-07 11:30:58 +0200

    added jobs

--- TandP/start_job.sh
+++ TandP/start_job.sh
@@ -3,7 +3,7 @@
 g++ -o DrawInvMassBkg_combi DrawInvMassBkg_combi.cc `root-config --cflags --glibs` -L $ROOTSYS/lib -lRooFit -lMinuit -lRooFitCore
 
 #test the code
-#qsub job.sh "ResultCombi2" 13 "" "reliso3" 0.15 "Pt" "eta" "dyonly" 
+qsub job.sh "ResultCombi2" 13 "" "reliso3" 0.15 "Pt" "eta" "dyonly" 
 
 #jobs for comparison with MINIAOD POG tnp
 #qsub job.sh "ResultCombi2" 13 "tightcut" "reliso3" 0.12 "Pt" "eta" "dyonly"
